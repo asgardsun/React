16d5ff6cd29c04446f2dacc564e33641867b9b4d
geun <geun@geunui-MacBookPro.local>
2022-01-26 14:57:20 +0900

    2021/01/26

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App(){ //function을 사용하여 함수같지만 이것은 컴포넌트이다.
+  return <div>Hello</div>;     //컴포넌트명의 첫번째 글자는 대문자로 작성해야 한다. -> JSX(javscript + html)
+}
+
+export default App;
