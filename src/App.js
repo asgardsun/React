58e64e0082cbcb3088279928a4efda348c83bb97
geun <asgardsun97@gmail.com>
2022-02-02 23:20:51 +0900

    2021/02/02 map() func

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import Device from './Device';
-
-function App(){ //function을 사용하여 함수같지만 이것은 컴포넌트이다.
-  return(
-  
- <div>
-  Hello
-  <Device mydevice="iPad Pro" myfavoritecharacter="Mickey Mouse"/>
-  <Device mydevice="iPhone Xs" myfavoritecharacter="iron man"/>
-  <Device mydevice="Apple Watch" myfavoritecharacter="Pooh"/>
-  <Device mydevice="MacBook Pro" myfavoritecharacter="Spider man"/>
-  <Device mydevice="AirPods Pro" myfavoritecharacter="Harry"/>
-  </div>     //컴포넌트명의 첫번째 글자는 대문자로 작성해야 한다. -> JSX(javscript + html)
-  );
+ 
+function App() {
+ 
+    return (
+        <div>
+            <Device />
+        </div>
+ 
+    );
 }
-
+ 
 export default App;
--- src/Device.js
+++ src/Device.js
@@ -1,8 +1,80 @@
-import React from 'react';
+// import React from 'react';
+//  //Map() 함수 사용
+// const mydata = {
+//     "myDeviceData":[
+//       {
+//         "name":"iPad Pro",
+//         "RAM":6,
+//         "HomeButton":false,
+//         "TouchID":"No",
+//         "FaceID":"Yes"
+//       },{
+//         "name":"iPhone Xs",
+//         "RAM":4,
+//         "HomeButton":false,
+//         "TouchID":"No",
+//         "FaceID":"Yes"
+//       },{
+//         "name":"iPhone 6",
+//         "RAM":1,
+//         "HomeButton":true,
+//         "TouchID":"Yes",
+//         "FaceID":"No"
+//       }
+//     ]
+// };
+ 
+// function Device() {
+//     return (
+//         <div>
+//             {
+//                 mydata.myDeviceData.map((myAppleDevice) => {
+//                     console.log('idx is ' + JSON.stringify(myAppleDevice.name));
+//                     return(
+//                         <div>
+//                             이름 : { myAppleDevice.name } <br></br>
+//                             램 : { myAppleDevice.RAM }GB<br></br>
+//                             홈버튼 : { ((myAppleDevice.HomeButton === true) ? '있음' : '없음') } <br></br>
+//                             터치 ID : { myAppleDevice.TouchID } <br></br>
+//                             페이스 ID : { myAppleDevice.FaceID } <br></br><br></br>
+//                          </div>
+//                     );
+//                 })
+//             }
+//         </div>
+//     );
+ 
+// }
+ 
+// export default Device;
 
-function Device(data){
-    console.log(JSON.stringify(data));
-    return <div>My {data.mydevice},{data.myfavoritecharacter}</div>;
+
+//함수형 컴포넌트 생성 방법
+import React, { Component } from 'react';
+ 
+class Device extends Component {
+    constructor(props) {
+        super(props);
+        console.log('in constructor');
+    }
+ 
+    componentDidMount() {
+        console.log('in componentDidMount');
+    }
+ 
+    componentDidUpdate() {
+        console.log('in componentDidUpdate');
+    }
+ 
+    componentWillUnmount() {
+        console.log('in componentWillUnmount');
+    }
+ 
+    render() {
+        return <div>Here is Device Component</div>;
+    }
 }
-
+ 
 export default Device;
+
+
